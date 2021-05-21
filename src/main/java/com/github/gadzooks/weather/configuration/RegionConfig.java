e857e1ec2dc3419fba0daba8f83891ea8b5dcab1
Amit Karwande <karwande@gmail.com>
2021-05-20 22:36:41 -0700

    load properties from application.yml

--- src/main/java/com/github/gadzooks/weather/configuration/RegionConfig.java
+++ src/main/java/com/github/gadzooks/weather/configuration/RegionConfig.java
@@ -10,10 +10,10 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,33 +21,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Configuration
+@Component
+@ConfigurationProperties(prefix = "regions", ignoreUnknownFields = false)
+@NoArgsConstructor
 public class RegionConfig {
 
     private static final Logger log = LoggerFactory.getLogger(RegionConfig.class);
-
-    @Bean
-    CommandLineRunner initDatabase() {
-        return args -> {
-            log.info("running initDatabase");
-        };
+    private String file;
+    public void setFile(final String fp) {
+        this.file = fp;
     }
 
-    public RegionConfig() throws IOException {
+//    @Bean
+//    CommandLineRunner initDatabase() {
+//        return args -> {
+//            log.info("running initDatabase");
+//        };
+//    }
+
+    @PostConstruct
+    public void init() throws IOException {
         List<Region> jsonRegions;
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory()); // create once, reuse
         CollectionType listType = mapper.getTypeFactory().constructCollectionType(
                 ArrayList.class, Region.class);
 
-        //FIXME read from application.properties
-        jsonRegions = mapper.readValue(new File("src/main/resources/regions.yml"), listType);
+        log.info("load regions from file : " + file);
+        jsonRegions = mapper.readValue(new File(file), listType);
 
         this.regions = ImmutableList.copyOf(jsonRegions);
 
         Map<String, Region> byIds = new HashMap<>();
 
         for (Region r: regions) {
-            log.debug("region is : " + r);
+            log.info("region is : " + r);
             byIds.put(r.name, r);
         }
         this.regionsById = ImmutableMap.copyOf(byIds);
@@ -55,8 +62,8 @@ public class RegionConfig {
 
     // NOTE: using Guava's immutable collection classes
     @Getter
-    private final ImmutableList<Region> regions;
-    private final ImmutableMap<String, Region> regionsById;
+    private ImmutableList<Region> regions;
+    private ImmutableMap<String, Region> regionsById;
 
     public Region getRegionByName(final String id) {
         return regionsById.get(id);
