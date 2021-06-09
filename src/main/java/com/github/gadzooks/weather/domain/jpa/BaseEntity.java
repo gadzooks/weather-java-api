c6b8f2874a13aa02cb9b4c059e564187345486c6
Amit Karwande <karwande@gmail.com>
2021-06-09 00:29:02 -0700

    BaseEntity implements Serializable

--- src/main/java/com/github/gadzooks/weather/domain/jpa/BaseEntity.java
+++ src/main/java/com/github/gadzooks/weather/domain/jpa/BaseEntity.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @Setter
-public class BaseEntity {
+public class BaseEntity implements Serializable {
     @Id
     //underlying will provide the id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
